6273f58a21bd69973f9de01b7c7ac196f2684c78
tomatorivera <tomatorivera@gmail.com>
2026-02-06 18:10:58 -0300

    Preparación del entorno de trabajo

---
+++ src/main/java/com/tocttaviano/crudclientes/app/CrudClientesSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.tocttaviano.crudclientes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesSpringmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tocttaviano/crudclientes/app/CrudClientesSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tocttaviano.crudclientes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
